978940b6f4801a06d76ffdb9dc25383c2afb7d7e
Nico <connie@MacBook-Pro.fritz.box>
2020-08-23 18:51:09 +0200

    first commit

---
+++ cmd/decode.go
@@ -0,0 +1,59 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+	"encoding/base64"
+	"github.com/spf13/cobra"
+)
+
+// decodeCmd represents the decode command
+var decodeCmd = &cobra.Command{
+	Use:   "decode",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// encodedString, _:= cmd.Flags().GetString("encodedString")
+		var encodedString string = args[0]
+		decoded, err := base64.StdEncoding.DecodeString(encodedString)
+		if err != nil {
+			fmt.Println("decode error:", err)
+			return
+		}
+		fmt.Printf("%q\n", decoded)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(decodeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// decodeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// decodeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// decodeCmd.Flags().StringP("encodedString", "s", "", "Help message for string")
+}
